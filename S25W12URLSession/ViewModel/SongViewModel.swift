704f25689656c631364811dd1262c5cdc707a2df
devbwoh <bwoh@outlook.kr>
2025-11-19 05:30:09 +0900

    🗃️DB 접근

---
+++ S25W12URLSession/ViewModel/SongViewModel.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+@MainActor
+@Observable
+class SongViewModel {
+    private var _songs: [Song] = []
+    var songs: [Song] { _songs }
+    
+    func loadSongs() async {
+        let requestURL = URL(string: SongApiConfig.serverURL)!
+        let (data, _) = try! await URLSession.shared.data(from: requestURL)
+        let decoder = JSONDecoder()
+        _songs = try! decoder.decode([Song].self, from: data)
+    }
+}
